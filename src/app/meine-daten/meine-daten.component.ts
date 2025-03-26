23ad4ec822e464d7bb4d11c248a790c99960b4c0
Jeta Hoxha <132713274+jetahox@users.noreply.github.com>
2025-03-26 14:09:06 +0100

    "Updated CSS files for layout adjustments and added imports"

--- src/app/meine-daten/meine-daten.component.ts
+++ src/app/meine-daten/meine-daten.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from "../footer/footer.component";
+
 
 @Component({
   selector: 'app-meine-daten',
   standalone: true,
   templateUrl: './meine-daten.component.html',
   styleUrl: './meine-daten.component.css',
-  imports: [CommonModule, FormsModule]
+  imports: [CommonModule, FormsModule, FooterComponent]
 })
 export class MeineDatenComponent {
   oldPassword = '';
