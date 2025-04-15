4bff57ab6b0968ecb389e0f6257c2b3ebee95939
Jeta Hoxha <132713274+jetahox@users.noreply.github.com>
2025-04-15 10:33:18 +0200

    Changed login error text styling, changed sidebar alarm item styling, added drag and drop functions in the Alarm kette component

--- src/app/alarm/alarmkette/alarmkette.component.ts
+++ src/app/alarm/alarmkette/alarmkette.component.ts
@@ -1,11 +1,50 @@
 import { Component } from '@angular/core';
 
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+  CdkDrag,
+  CdkDropList,
+  DragDropModule
+} from '@angular/cdk/drag-drop';
+
 @Component({
   selector: 'app-alarmkette',
-  imports: [],
+  standalone: true,
+  imports: [CdkDropList, CdkDrag, DragDropModule],
   templateUrl: './alarmkette.component.html',
-  styleUrl: './alarmkette.component.css'
+  styleUrl: './alarmkette.component.css',
 })
 export class AlarmketteComponent {
+  todo = [
+    { name: 'Trudo Qa', avatar: '/assets/avatar1.png' },
+    { name: 'Ylber Pllana', avatar: '/assets/avatar2.png' }
+  ];
+  
+  done = [
+    { name: 'Trudo', avatar: '/assets/avatar2.png' }
+  ];
+  
 
+  drop(event: CdkDragDrop<any[]>) {
+    // moving inside the todolist
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data, 
+        event.previousIndex, 
+        event.currentIndex);
+    } 
+    // moving in the done list
+    else {
+
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
+  
 }
