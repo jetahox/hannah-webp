68811030bd01113c2ec2412710bf2e71a161d75a
Jeta Hoxha <132713274+jetahox@users.noreply.github.com>
2025-04-15 15:23:48 +0200

    Added functionality to the pop up modal

--- src/app/alarm/alarmkette/alarmkette.component.ts
+++ src/app/alarm/alarmkette/alarmkette.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { NgClass } from '@angular/common';
-import { NgIf } from '@angular/common';
+import { NgClass, NgIf } from '@angular/common';
 import {
   CdkDragDrop,
   moveItemInArray,
@@ -13,7 +12,7 @@ import {
 @Component({
   selector: 'app-alarmkette',
   standalone: true,
-  imports: [CdkDropList, CdkDrag, DragDropModule,NgClass,NgIf],
+  imports: [CdkDropList, CdkDrag, DragDropModule, NgClass, NgIf],
   templateUrl: './alarmkette.component.html',
   styleUrl: './alarmkette.component.css',
 })
@@ -22,68 +21,76 @@ export class AlarmketteComponent {
     { name: 'Trudo Qa', avatar: '/assets/avatar1.png' },
     { name: 'Ylber Pllana', avatar: '/assets/avatar2.png' }
   ];
-  
+
   done = [
     { name: 'Trudo', avatar: '/assets/avatar2.png' }
   ];
 
   showModal = false;
-  itemToMove: any=null;
-  originalContainer: any [] = [];
+  itemToMove: any = null;
+  originalContainer: any[] = [];
+  targetContainer: any[] = [];
+  originalIndex: number = -1;
+  targetIndex: number = -1;
 
-  drop(event:CdkDragDrop<any[]>){
-    // Reset the hover states
-    this.isHoveringOverDoneList=false;
-    this.isHoveringOverTodoList=false;
+  isHoveringOverDoneList = false;
+  isHoveringOverTodoList = false;
+
+  drop(event: CdkDragDrop<any[]>) {
+    this.isHoveringOverDoneList = false;
+    this.isHoveringOverTodoList = false;
+
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+      return;
+    }
+
+    this.itemToMove = event.previousContainer.data[event.previousIndex];
+    this.originalContainer = event.previousContainer.data;
+    this.targetContainer = event.container.data;
+    this.originalIndex = event.previousIndex;
+    this.targetIndex = event.currentIndex;
+
+    transferArrayItem(
+      this.originalContainer,
+      this.targetContainer,
+      this.originalIndex,
+      this.targetIndex
+    );
 
     this.showModal = true;
-    this.itemToMove = event.item.data;
-    this.originalContainer = event.previousContainer.data; 
-     // moving inside the todolist
-     if (event.previousContainer === event.container) {
-      moveItemInArray(
-        event.container.data, 
-        event.previousIndex, 
-        event.currentIndex);
-    } 
-    // moving in the done list
-    else {
-
-      transferArrayItem(
-        event.previousContainer.data,
-        event.container.data,
-        event.previousIndex,
-        event.currentIndex
-      );
-    }
   }
-  isHoveringOverDoneList = false;
-  onEnterDoneList(){
+
+  onEnterDoneList() {
     this.isHoveringOverDoneList = true;
   }
-  onExitDoneList(){
+
+  onExitDoneList() {
     this.isHoveringOverDoneList = false;
   }
-  isHoveringOverTodoList = false;
 
   onEnterTodoList() {
     this.isHoveringOverTodoList = true;
   }
-  
+
   onExitTodoList() {
     this.isHoveringOverTodoList = false;
   }
+
   confirmTransfer(confirmed: boolean) {
-    if (confirmed) {
-    } else {
-      
-      const index = this.originalContainer.findIndex(item => item === this.itemToMove);
-      if (index !== -1) {
-        this.originalContainer.splice(index, 0, this.itemToMove);
+    if (!confirmed && this.itemToMove && this.originalContainer && this.targetContainer) {
+      const removedIndex = this.targetContainer.findIndex(item => item === this.itemToMove);
+      if (removedIndex !== -1) {
+        this.targetContainer.splice(removedIndex, 1);
+        this.originalContainer.splice(this.originalIndex, 0, this.itemToMove);
       }
     }
-    this.showModal = false; 
-    this.itemToMove = null; 
-  }
-  }
 
+    this.showModal = false;
+    this.itemToMove = null;
+    this.originalContainer = [];
+    this.targetContainer = [];
+    this.originalIndex = -1;
+    this.targetIndex = -1;
+  }
+}
